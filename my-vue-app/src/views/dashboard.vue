1ff60ba89005228e2c29be8039801c2d8506c3b8
CathLee <447932704@qq.com>
2024-03-12 23:47:41 +0800

    test:测试奇怪方法的可行性

--- my-vue-app/src/views/dashboard.vue
+++ my-vue-app/src/views/dashboard.vue
@@ -1,24 +1,29 @@
 <!--
  * @Author: cathylee 447932704@qq.com
  * @Date: 2022-12-03 14:26:08
- * @LastEditors: cathylee 447932704@qq.com
- * @LastEditTime: 2023-11-25 21:49:34
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2024-03-12 23:45:44
  * @FilePath: /my-vue-app/src/views/dashboard.vue
  * @Description: 
  * 
  * Copyright (c) 2022 by cathylee 447932704@qq.com, All Rights Reserved. 
 -->
 <template>
-  <div class="title">欢迎登陆后台管理系统</div>
+  <div class="title" data-logger-id="test" @click="doAll">欢迎登陆后台管理系统</div>
 </template>
 
 <script setup lang="ts">
 import { getCurrentInstance, inject, onMounted } from 'vue';
 const proxy = getCurrentInstance()?.proxy;
-onMounted(()=>{
+const a = () => console.log("哈哈哈哈")
+const doAll = (e)=>{
+  track(e)
+  a()
+}
+onMounted(() => {
   const globalParam = inject('globalParam')
   console.log("this is :", globalParam);
-  
+
 })
 </script>
 
