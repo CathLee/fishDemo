53c26d4ed749e8c183a23a904ef0f09667f2117d
CathLee <4447932704@qq.com>
2025-02-01 16:09:41 +0800

    fix:change image path

--- my-react-project/src/data/artworks.ts
+++ my-react-project/src/data/artworks.ts
@@ -3,44 +3,44 @@ import { ArtPiece } from '../types/types';
 export const artworks: ArtPiece[] = [
     {
         id: 1,
-        title: "抽象构图",
+        title: "抽象构图", 
         year: "1955年作品",
-        imageUrl: "../../../downloaded_images/huile-03.jpg"
+        imageUrl: "/downloaded_images/huile-03.jpg"
     },
     {
         id: 2,
         title: "光与色的交响",
-        year: "1962年作品",
-        imageUrl: "../../../downloaded_images/huile-02.jpg"
+        year: "1962年作品", 
+        imageUrl: "/downloaded_images/huile-02.jpg"
     },
     {
         id: 3,
         title: "无题",
         year: "1970年作品",
-        imageUrl: "../../../downloaded_images/portrait-de-ma-femme.jpg"
+        imageUrl: "/downloaded_images/portrait-de-ma-femme.jpg"
     },
     {
         id: 4,
         title: "无题",
         year: "1970年作品",
-        imageUrl: "../../../downloaded_images/huile-08.jpg"
+        imageUrl: "/downloaded_images/huile-08.jpg"
     },
     {
         id: 5,
         title: "无题",
         year: "1970年作品",
-        imageUrl: "../../../downloaded_images/huile-07.jpg"
+        imageUrl: "/downloaded_images/huile-07.jpg"
     },
     {
         id: 6,
         title: "无题",
         year: "1970年作品",
-        imageUrl: "../../../downloaded_images/huile-09.jpg"
+        imageUrl: "/downloaded_images/huile-09.jpg"
     },
     {
         id: 7,
         title: "无题",
         year: "1970年作品",
-        imageUrl: "../../../downloaded_images/huile-10.jpg"
+        imageUrl: "/downloaded_images/huile-10.jpg"
     },
 ]; 
